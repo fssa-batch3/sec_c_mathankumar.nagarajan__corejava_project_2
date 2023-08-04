d6c1e66b3ee3f8c58cfff42451c1c68fbff247bd
Mathankumar G N <116251925+mathangn@users.noreply.github.com>
2023-08-04 18:32:21 +0530

    Add files via upload

--- src/main/java/com/fssa/spartansmt/dao/ConnectionUtil.java
+++ src/main/java/com/fssa/spartansmt/dao/ConnectionUtil.java
@@ -4,23 +4,38 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class ConnectionUtil {
 
 	public static Connection getConnection() throws SQLException {
-		
+
 		Connection con = null;
-		String url = "jdbc:mysql://localhost:3306/spartans_mt";
-        String userName = "root";
-        String passWord = "123456";
-        
-        try {
-        	con = DriverManager.getConnection(url, userName, passWord);
-        } catch(SQLException ex) {
-        	ex.printStackTrace();
-        	throw new RuntimeException("Unable to connect to the database");
-        }
-        return con;
+
+		Dotenv env = Dotenv.load();
+		String host = env.get("DATABASE_HOST");
+		String username = env.get("DATABASE_USERNAME");
+		String password = env.get("DATABASE_PASSWORD");
+
+		try {
+			con = DriverManager.getConnection(host, username, password);
+			System.out.println("Connection successfull");
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			throw new RuntimeException("Unable to connect to the database");
+		}
+		return con;
 
 	}
-	
+
+	public static void main(String[] args) {
+
+		try {
+			Connection con = getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
