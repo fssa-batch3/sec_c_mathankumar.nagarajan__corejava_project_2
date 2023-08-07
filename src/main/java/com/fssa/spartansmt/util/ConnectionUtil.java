f9757811d52154269df8d3f93426eaf26cb33fad
Mathankumar G N <116251925+mathangn@users.noreply.github.com>
2023-08-08 00:08:23 +0530

    Add files via upload

--- src/main/java/com/fssa/spartansmt/util/ConnectionUtil.java
+++ src/main/java/com/fssa/spartansmt/util/ConnectionUtil.java
@@ -5,38 +5,38 @@ import java.sql.DriverManager;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
-//import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConnectionUtil {
-public static void main(String[] args) {
-	getConnection();
-	System.out.println("working");
-}
-	   public static Connection getConnection() {
-	        Connection con = null;
 
-	        String url, userName, passWord;
+	public static Connection getConnection() {
+		Connection con = null;
 
-	        if (System.getenv("CI") != null) {
-	            url = System.getenv("DATABASE_HOST");
-	            userName = System.getenv("DATABASE_USERNAME");
-	            passWord = System.getenv("DATABASE_PASSWORD");
-	        } else {
-	            Dotenv env = Dotenv.load();
-	            url = env.get("DATABASE_HOST");
-	            userName = env.get("DATABASE_USERNAME");
-	            passWord = env.get("DATABASE_PASSWORD");
-	        }
+		String url, userName, passWord;
 
-	        try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            con = DriverManager.getConnection(url, userName, passWord);
-	            System.out.println("Connected");
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            throw new RuntimeException("Unable to connect to the database");
-	        }
-	        return con;
-	    }
+		if (System.getenv("CI") != null) {
+			url = System.getenv("DATABASE_HOST");
+			userName = System.getenv("DATABASE_USERNAME");
+			passWord = System.getenv("DATABASE_PASSWORD");
+		} else {
+			Dotenv env = Dotenv.load();
+			url = env.get("DATABASE_HOST");
+			userName = env.get("DATABASE_USERNAME");
+			passWord = env.get("DATABASE_PASSWORD");
+		}
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(url, userName, passWord);
+			System.out.println("Connected");
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to connect to the database");
+		}
+		return con;
+	}
+	
+	public static void main(String[] args) {
+		getConnection();
+	}
 
 }
