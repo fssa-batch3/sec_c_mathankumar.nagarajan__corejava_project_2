76a632909ea0d8de74879b0ff004bd45b4ee71c9
Mathankumar G N <116251925+mathangn@users.noreply.github.com>
2023-12-05 22:04:27 +0530

    Updated SQL Script

--- src/main/sql/sqlscripts.sql
+++ src/main/sql/sqlscripts.sql
@@ -1,6 +1,5 @@
-Use mathankumar_nagarajan_corejava_project;
+USE mathankumar_nagarajan_corejava_project;
 
--- Store Table Details
 CREATE TABLE IF NOT EXISTS stores(
 	store_id int primary key AUTO_INCREMENT,
     store_name VARCHAR (75) NOT NULL,
@@ -8,8 +7,11 @@ CREATE TABLE IF NOT EXISTS stores(
     store_logo blob NOT NULL
     );
     
--- Product Table Detaisl
-CREATE TABLE IF NOT EXISTS products (
+    
+    
+    select * from stores;
+
+    CREATE TABLE IF NOT EXISTS products (
 	product_id INT PRIMARY KEY AUTO_INCREMENT,
     product_title VARCHAR(500) NOT NULL,
     product_price DOUBLE NOT NULL,
@@ -19,30 +21,56 @@ CREATE TABLE IF NOT EXISTS products (
     foreign key(store_id) references stores (store_id)
 );
 
+select * from products;
+SELECT product_id, product_title, product_price, product_image, uploaded_date, store_id FROM products WHERE product_id = 2;
+select * from products where store_id = 2;
 
--- User Table Details
 CREATE TABLE IF NOT EXISTS user(
 	user_id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR (100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(200) NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    phone_number BIGINT NOT NULL
+    email VARCHAR(200) UNIQUE NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    phone_number BIGINT NOT NULL,
+    address VARCHAR(500) NULL,
+    country VARCHAR(100) NULL,
+    state VARCHAR(100) NULL,
+    zipcode INT NULL,
+    role VARCHAR(25) NOT NULL
 );
 
+select * from user;
+select user_id, first_name, last_name, email, password, phone_number from user where email = 'mathan@gmail.com';
 
--- Orders Table Details
+select user_id from user where email = 'mathan@gmail.com';
+
 CREATE TABLE IF NOT EXISTS orders (
 	order_id INT PRIMARY KEY AUTO_INCREMENT,
 	user_id int not null,
-    payment_status VARCHAR(10) NOT NULL,
+    payment_option VARCHAR(50) NOT NULL,
     total_price INT NOT NULL,
     ordered_date date not null,
+    
+    shipping_address VARCHAR(500) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    zip_code INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 
--- Ordered Product Table Details
+select * from orders;
+
+select * from orders where user_id = 4;
+
+SELECT * FROM orders WHERE user_id = 1 ORDER BY order_id DESC LIMIT 1;
+
+
+
+select orders.order_id, orders.user_id, orders.payment_option, orders.total_price, orders.ordered_date, ordered_products.ordered_product_id, 
+ordered_products.product_id, ordered_products.quantity from orders inner join ordered_products on orders.order_id = ordered_products.order_id where user_id = 2;
+
+
 CREATE TABLE IF NOT EXISTS ordered_products (
 	ordered_product_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT NOT NULL,
@@ -51,4 +79,23 @@ CREATE TABLE IF NOT EXISTS ordered_products (
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
+
+select * from ordered_products;
+
+select * from ordered_products where order_id = 1;
+
+-- For Number of Count Sales Analytics Query
+SELECT ordered_date, COUNT(order_id) 
+FROM orders
+GROUP BY ordered_date 
+ORDER BY ordered_date DESC LIMIT 5;
+
+-- Revenue Analytics Query
+SELECT ordered_date, sum(total_price)
+FROM orders
+GROUP BY ordered_date
+ORDER BY ordered_date DESC LIMIT 5;
+
+
+
 
